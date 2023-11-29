4addd706797e5d569d39dd73ddfc573e21aa7491
imjazzdev <jazzdev01@gmail.com>
2023-11-29 19:31:43 +0700

    <3

---
+++ lib/pages/pembayaran.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class PembayaranPage extends StatelessWidget {
+  const PembayaranPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SizedBox(
+        height: double.maxFinite,
+        width: double.maxFinite,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Payment via QR CODE DANA',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Image.asset('assets/dana.jpeg')
+          ],
+        ),
+      ),
+    );
+  }
+}
